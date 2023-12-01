de279a57ea688297d94b3bc3c8ad3aaf190a4603
Banjirsultana90 <banjirsultana15@gmail.com>
2023-12-01 15:42:11 +0600

    modified

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
+
 const cookieParser = require('cookie-parser')
 const moment = require('moment');
-require('dotenv').config()
+
 app.use(cors(
     {
         origin: [
@@ -24,9 +27,9 @@ const logger = async (req, res, next) => {
     next()
 }
 
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { default: Stripe } = require('stripe');
+const { Await } = require('react-router-dom');
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASSWORD}@cluster0.id4vsgi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +48,7 @@ async function run() {
     const allcreatedsurvey = client.db('surveyDB').collection('allcreatedsurvey')
     const allvotedfeature = client.db('surveyDB').collection('allvotedfeature')
     const alluser = client.db('surveyDB').collection('alluser')
+    const paymentcollection = client.db('surveyDB').collection('paymentcollection')
 
 
     const verifyToken = async (req, res, next) => {
@@ -61,14 +65,9 @@ async function run() {
             req.decoded = decoded
             
              next()
-    
-            
         })
-       
     
     }
-
-
     const verifyAdmin = async (req, res, next) => {
         const email = req.decoded.email;
         const query = { email: email };
@@ -85,7 +84,7 @@ async function run() {
     // auth api
     app.post('/jwt', async (req, res) => {
         const user = req.body
-        console.log('user for token', user);
+        // console.log('user for token', user);
         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
         res.cookie('token', token, {
             httpOnly: true,
@@ -130,7 +129,7 @@ async function run() {
     });
     
     
-    app.get('/alluser', verifyToken, verifyAdmin, async (req, res) => {
+    app.get('/alluser', verifyToken,async (req, res) => {
         const result = await alluser.find().toArray();
         res.send(result);
     });
@@ -168,7 +167,7 @@ async function run() {
     });
     
 
-    app.post('/alluser', async (req, res) => {
+    app.post('/alluser',verifyToken, async (req, res) => {
         const user = req.body;
         const query={email:user.email}
         const existinguser=await alluser.findOne(query)
@@ -178,20 +177,64 @@ async function run() {
         const result = await alluser.insertOne(user)
         res.send(result)
     })
-
+// vote related api
 
     app.post('/allvotedfeature', verifyToken, async (req, res) => {
         const voteRecord = req.body;
         console.log(voteRecord);
-        // console.log ('token owner info',req.user);
-        // if(req.user.email!==req.query.email){
-        //     return res.status(403).send({message:'forbidden access'})
-        // }
         const timestamp = moment().format();
         voteRecord.timestamp = timestamp;
         const result = await allvotedfeature.insertOne(voteRecord)
         res.send(result)
     })
+    // app.get('/allvotedfeature/:email/:id',verifyToken, async (req, res) => {
+    //     const { email, id } = req.params;
+    //     console.log(email,id);
+    //     const existingVote = await allvotedfeature.findOne({ userId: email, surveyId: id });
+
+    //     if (existingVote) {
+    //         res.status(200).json({ hasVoted: true });
+    //     } else {
+    //         res.status(200).json({ hasVoted: false });
+    //     }
+    // })
+
+    app.get('/allvotedfeature/:email/:id', verifyToken, async (req, res) => {
+        try {
+            const { email, id } = req.params;
+    
+            console.log('Received params:', email, id);
+    
+            const existingVote = await allvotedfeature.findOne({ email: email, surveyId: id });
+    
+            console.log('Existing vote:', existingVote);
+    
+            if (existingVote) {
+                res.status(200).json({ hasVoted: true });
+            } else {
+                res.status(200).json({ hasVoted: false });
+            }
+        } catch (error) {
+            console.error('Error while checking existing votes:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+    
+    // app.get('/allvotedfeature/:email/:id', verifyToken, async (req, res) => {
+    //     const { email, id } = req.params;
+    //     console.log(email, id);
+    //     try {
+    //         const existingVotes = await allvotedfeature.find({ userId: email, surveyId: id });
+            
+    //         if (existingVotes.length > 0) {
+    //             res.status(200).json({ hasVoted: true });
+    //         } else {
+    //             res.status(200).json({ hasVoted: false });
+    //         }
+    //     } catch (error) {
+    //         console.error('Error while checking existing votes:', error);
+    //       
+    
 
     app.get('/allcreatedsurvey', async (req, res) => {
         
@@ -208,9 +251,86 @@ async function run() {
         const result = await allcreatedsurvey.insertOne(formdata)
         res.send(result)
     })
+ 
+    app.post("/create-payment-intent", async (req, res) => {
+        const { price } = req.body;
+        console.log("Received price:", price); 
     
+        const amount = parseInt(price * 100);
+        // console.log('getttttt',amount);
+    
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+    
+            console.log("Client secret generated:", paymentIntent.client_secret); // Log the client_secret
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        } catch (error) {
+            console.error("Error creating payment intent:", error.message); // Log any errors
+            res.status(500).send({ error: error.message });
+        }
+    });
 
-   
+    app.get('/payments',verifyToken,verifyAdmin,async(req,res)=>{
+        const result=await paymentcollection.find().toArray()
+        res.send(result)
+
+    })
+    
+    app.get('/payments/:email',verifyToken,async(req,res)=>{
+        const query={email:req.params.email}
+        if(req.params.email!==req.decoded.email){
+            return req.status(403).send({message:'access forbidden'})
+        }
+        const result=await paymentcollection.find(query).toArray()
+        res.send(result)
+
+    })
+    
+// for pro user:to do
+
+
+    // app.post ('/payments',async(req,res)=>{
+    //     const payment=req.body
+    //     paymentresult=await paymentcollection.insertOne(payment)
+    //     console.log('payment info',payment);
+    //     const query={
+    //         $in:payment.email
+
+    //     }
+    //     const updateuserrole=await alluser.updateOne(query)
+    //     res.send(paymentresult,updateuserrole)
+    // })
+    app.post('/payments', async (req, res) => {
+        const payment = req.body;
+    
+        // Insert payment information into payment collection
+        const paymentResult = await paymentcollection.insertOne(payment);
+        console.log('Payment info', payment);
+    
+        // Check if email exists in the payment object
+        if (payment.email) {
+            // Define the query to find the user by email
+            const query = { email: payment.email };
+    
+            // Update user role in alluser collection
+            const updatedUser = await alluser.updateOne(query, {
+                $set: { role: 'prouser' /* Set the role to the desired value */ }
+            });
+    
+            // Send response with payment result and update result
+            res.send({ paymentResult, updatedUser });
+        } else {
+            // Send response with only payment result if email is not found
+            res.send({ paymentResult });
+        }
+    });
+    
 
     try {
 
